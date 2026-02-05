d5954182dce1501a343ec0560d1b6485c3c70ed9
hellomello222 <hanishabitt@gmail.com>
2026-02-05 23:52:29 +0800

    Add files via upload

---
+++ LAB1/LAB1TASK2.R
@@ -0,0 +1,13 @@
+# Given variables
+num1 <- 0.956786
+num2 <- 7.8345901
+
+# Round num1 to 2 decimal places
+num1_rounded <- round(num1, 2)
+
+# Round num2 to 3 decimal places
+num2_rounded <- round(num2, 3)
+
+# Display the results
+print(num1_rounded)
+print(num2_rounded)
---
+++ LAB1/LAB1TASK3.R
@@ -0,0 +1,8 @@
+# Retrieve radius from user
+radius <- as.numeric(readline(prompt = "Enter the radius of the circle: "))
+
+# Calculate the area of the circle
+area <- pi * radius^2
+
+# Display the area
+print(area)
